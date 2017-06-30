37582f3f209de8fe0bc4ca7966cac4f632b77f40
rhysgoehring <rhysgoehring@gmail.com>
2017-06-29 18:34:43 -0600

    added route for getting comments by id

--- routes/lapses.js
+++ routes/lapses.js
@@ -11,6 +11,19 @@ router.get('/', (req, res, next) => {
 router.get('/comments', (req ,res ,next) => {
   knex('comments').returning('*').then((comments)=> res.json(comments))
   .catch((err) => next(err));
-})
+});
+
+router.get('/comments/:id', (req ,res ,next) => {
+  console.log('in comments:id')
+  const id = req.params.id
+  return knex('comments')
+    .select('*')
+    .where('lapse_id', id)
+    .first()
+    .then((comments) => res.json(comments))
+    .catch((err) => next(err));
+});
+
+
 
 module.exports = router;
