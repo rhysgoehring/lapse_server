f4771b149a81daacf17cfcf44bec11d2c05ce485
rhysgoehring <rhysgoehring@gmail.com>
2017-07-05 15:12:59 -0600

    deploy

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  
 });
 
 module.exports = app;
