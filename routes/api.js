b6de6eb2b398ffa68f95aebf183a147b9f3b13a0
rhysgoehring <rhysgoehring@gmail.com>
2017-07-05 12:51:22 -0600

    deploy again

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const cors = require('cors');
+
 const express = require('express');
 const router = express.Router();
 const index = require('./index');
 const users = require('./users');
 const lapses = require('./lapses');
 
-app.use(cors());
-app.options('*', cors());
+
 
 router.use('/', index);
 router.use('/users', users);
