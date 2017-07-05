aba168b4db4fee77a6d3ee3aaaa6da69a8472268
rhysgoehring <rhysgoehring@gmail.com>
2017-07-05 12:25:45 -0600

    redeploy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const api = require('./routes/api');
 const app = express();
 app.use(cors());
 app.options('*', cors());
-app.use(bodyParser.json({ type: '*/*' }));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
