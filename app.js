499b837138b0ac84993c31735f944c4a74b6f032
rhysgoehring <rhysgoehring@gmail.com>
2017-07-05 12:36:36 -0600

    added another entry point in start script

--- app.js
+++ app.js
@@ -3,27 +3,28 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 
 
 
 const api = require('./routes/api');
 const app = express();
-
+app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json({ type: '*/*' }));
-app.use(bodyParser.urlencoded({ extended: false }));
+
 
 
 
 
 app.use(logger('dev'));
 
-app.use('/api', api)
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use('/api', api)
 
 
 app.use(function(err, req, res, next) {
