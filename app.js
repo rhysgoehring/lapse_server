97f267750ff71fe913a1499b356034799e537f0b
rhysgoehring <rhysgoehring@gmail.com>
2017-07-05 12:28:28 -0600

    deploying...

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+
 
 
 
 const api = require('./routes/api');
 const app = express();
-app.use(cors());
-app.options('*', cors());
-app.use(bodyParser.json());
+
+app.use(bodyParser.json({ type: '*/*' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
