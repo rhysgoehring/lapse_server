a57d8b8864470cc5bde4c513b1df005aa025aa53
rhysgoehring <rhysgoehring@gmail.com>
2017-07-05 13:31:14 -0600

    deploying

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
