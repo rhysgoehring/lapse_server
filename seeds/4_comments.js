d2fe659eb0ef20863c5ba3c8b7e8f7b0947e6326
rhysgoehring <rhysgoehring@gmail.com>
2017-07-05 10:05:57 -0600

    added comments

--- seeds/4_comments.js
+++ seeds/4_comments.js
@@ -16,6 +16,36 @@ exports.seed = function(knex) {
         commenter: 'boulderBro',
         lapse_id: 1,
         body: 'Dude, I think this is Instagram. Still trying to figure it out. What\'s up Mr. President?! We have the same barber, bro.'
+        },
+        {
+        commenter: 'rhysgoehring',
+        lapse_id: 4,
+        body: 'Hmm... That looks awefully familiar. Where exactly did you take this?'
+        },
+        {
+        commenter: 'boulderBro',
+        lapse_id: 4,
+        body: 'Try taking a video longer than like 10 seconds bro this isn\'t Snapchat... Is it?'
+        },
+        {
+        commenter: 'donaldtrump',
+        lapse_id: 4,
+        body: 'rhysgoehring, I know what you\'re thinking, I always do. I did not break into your apartment to take this. I am a man of the law, no one likes the law more than me, no one.'
+        },
+        {
+        commenter: 'rhysgoehring',
+        lapse_id: 5,
+        body: 'Please just stop breaking into my apartment, there\'s info on this site about how you can make your own time lapses without breaking in to my place.'
+        },
+        {
+        commenter: 'donaldtrump',
+        lapse_id: 5,
+        body: 'I agree rhysgoehring, that is a tasteless move. Shame on you BoulderBro, you were never my friend.'
+        },
+        {
+        commenter: 'boulderBro',
+        lapse_id: 5,
+        body: 'Dude. Donald. You were with me when I did it, bro. Remember, right after we got our haircuts?'
         }
     ]);
     }).then(() => {
